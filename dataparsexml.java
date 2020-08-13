52b3e9688f65ba84ac010b63b3745db1017bf387
indu-md <69618534+indu-md@users.noreply.github.com>
2020-08-13 12:15:22 +0530

    parse xml data code added

---
+++ dataparsexml.java
@@ -0,0 +1,27 @@
+package xmlParseExcel;
+
+public class dataparsexml {
+
+	public static void main(String[] args) throws Exception {
+		    exceldata obj=new exceldata();
+		
+			//System.out.println(obj.readalldata());
+		   
+		   
+
+			
+			//System.out.println(obj.getValue("TC SL"));
+			//System.out.println(obj.getValue("Description"));
+			//System.out.println(obj.getValue("Value"));
+			//System.out.println(obj.getValue("Title"));
+			//System.out.println(obj.getValue("Browser"));
+			//System.out.println(obj.getValue("Platform"));
+			//obj.CreateXmlFile();
+			//obj.readxml();
+			//obj.count();
+			
+		}
+
+	}
+
+
